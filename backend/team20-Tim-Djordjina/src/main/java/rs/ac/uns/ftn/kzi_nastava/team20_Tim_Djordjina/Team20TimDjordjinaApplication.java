32b666edf1593e569c2aca4c6510693a8d647c9b
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2026-01-22 02:00:46 +0100

    Initialized Spring Boot Application in backend

---
+++ backend/team20-Tim-Djordjina/src/main/java/rs/ac/uns/ftn/kzi_nastava/team20_Tim_Djordjina/Team20TimDjordjinaApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.kzi_nastava.team20_Tim_Djordjina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team20TimDjordjinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team20TimDjordjinaApplication.class, args);
+	}
+
+}
---
+++ backend/team20-Tim-Djordjina/src/test/java/rs/ac/uns/ftn/kzi_nastava/team20_Tim_Djordjina/Team20TimDjordjinaApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.kzi_nastava.team20_Tim_Djordjina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team20TimDjordjinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
