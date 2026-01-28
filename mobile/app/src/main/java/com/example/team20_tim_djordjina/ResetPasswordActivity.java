5d4b7d7c394ee1f958d7437d425bbfd8c6e156e0
AtinaF <folic.sv87.2023@uns.ac.rs>
2026-01-28 18:44:28 +0100

    added password reset ui

---
+++ mobile/app/src/main/java/com/example/team20_tim_djordjina/ResetPasswordActivity.java
@@ -0,0 +1,29 @@
+package com.example.team20_tim_djordjina;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_reset_password);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.reset_password_root), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onResetPasswordClick(View view) {
+        // TODO: send reset email later
+    }
+}
