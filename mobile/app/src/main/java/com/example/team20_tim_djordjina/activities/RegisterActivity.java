a5b737ebc619ae2e5ad0b8bef2f3592b8cb04b59
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2026-01-28 15:05:15 +0100

    Initialized views using binding and setted Up country code spinner in RegisterActivity

--- mobile/app/src/main/java/com/example/team20_tim_djordjina/activities/RegisterActivity.java
+++ mobile/app/src/main/java/com/example/team20_tim_djordjina/activities/RegisterActivity.java
@@ -1,6 +1,11 @@
 package com.example.team20_tim_djordjina.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +14,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.team20_tim_djordjina.R;
+import com.example.team20_tim_djordjina.databinding.ActivityRegisterBinding;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
+    private TextInputEditText etEmail, etPassword, etConfirmPassword, etFirstName, etLastName, etAddress;
+    private EditText etPhoneNumber;
+    private Spinner spinnerCountryCode;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("Team20_Tim_Djordjina", "Register Activity onCreate()");
         //EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -24,5 +39,36 @@ public class RegisterActivity extends AppCompatActivity {
             return insets;
         });
          */
+
+        // Initialize views
+        initViews();
+
+        // Setup country code spinner
+        setupCountryCodeSpinner();
+
+
+    }
+
+    private void setupCountryCodeSpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.country_codes,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCountryCode.setAdapter(adapter);
+        // Set default selection for +381
+        spinnerCountryCode.setSelection(0);
+    }
+
+    private void initViews() {
+        etEmail = binding.etEmail;
+        etPassword = binding.etPassword;
+        etConfirmPassword = binding.etConfirmPassword;
+        etFirstName = binding.etFirstName;
+        etLastName = binding.etLastName;
+        etAddress = binding.etAddress;
+        etPhoneNumber = binding.etPhoneNumber;
+        spinnerCountryCode = binding.spinnerCountryCode;
+        btnSignUp = binding.btnSignUp;
     }
 }
