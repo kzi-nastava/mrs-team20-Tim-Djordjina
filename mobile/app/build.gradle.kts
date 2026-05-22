c2875c3292668671f02168528923f494fb52b631
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2026-05-22 10:38:48 +0200

    Add dependencies for Retrofit to build.gradle; Added Internet permission to AndroidManifest.xml; Added Network Security Configuration.

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -43,6 +43,11 @@ dependencies {
     // For Material Design Components (TextInputLayout, etc.)
     implementation("com.google.android.material:material:1.11.0")
 
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.12.1")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
