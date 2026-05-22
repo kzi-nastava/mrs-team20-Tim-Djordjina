9150460c3dd6245e152bdd24649d765a0d5b4739
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2026-05-22 11:10:23 +0200

    Created Response DTO (ApiResponse.java); Changed return attributes in getters in RegistrationRequest.java from 'return this.attribute' to 'return attribute'.

--- mobile/app/src/main/java/com/example/team20_tim_djordjina/model/RegistrationRequest.java
+++ mobile/app/src/main/java/com/example/team20_tim_djordjina/model/RegistrationRequest.java
@@ -22,18 +22,18 @@ public class RegistrationRequest {
     }
 
     // Getters and Setters
-    public String getFirstName(){ return this.firstName; }
+    public String getFirstName(){ return firstName; }
     public void setFirstName(String firstName){ this.firstName = firstName; }
-    public String getLastName(){ return this.lastName; }
+    public String getLastName(){ return lastName; }
     public void setLastName(String lastName){ this.lastName = lastName; }
-    public String getEmail(){ return this.email; }
+    public String getEmail(){ return email; }
     public void setEmail(String email){ this.email = email; }
-    public String getPassword(){ return this.password; }
+    public String getPassword(){ return password; }
     public void setPassword(String password){ this.password = password; }
-    public String getConfirmPassword(){ return this.confirmPassword; }
+    public String getConfirmPassword(){ return confirmPassword; }
     public void setConfirmPassword(String confirmPassword){ this.confirmPassword = confirmPassword; }
-    public String getPhoneNumber(){ return this.phoneNumber; }
+    public String getPhoneNumber(){ return phoneNumber; }
     public void setPhoneNumber(String phoneNumber){ this.phoneNumber = phoneNumber; }
-    public String getAddress(){ return this.address; }
+    public String getAddress(){ return address; }
     public void setAddress(String address){ this.address = address; }
 }
