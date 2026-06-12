c4967d0312fe4c654c26387691200e5f60eccce0
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2026-06-12 13:31:32 +0200

    Added login and logout endpoint to ApiService

--- mobile/app/src/main/java/com/example/team20_tim_djordjina/api/ApiService.java
+++ mobile/app/src/main/java/com/example/team20_tim_djordjina/api/ApiService.java
@@ -1,6 +1,8 @@
 package com.example.team20_tim_djordjina.api;
 
 import com.example.team20_tim_djordjina.model.ApiResponse;
+import com.example.team20_tim_djordjina.model.LoginRequest;
+import com.example.team20_tim_djordjina.model.LoginResponse;
 import com.example.team20_tim_djordjina.model.RegistrationRequest;
 
 import retrofit2.Call;
@@ -10,6 +12,12 @@ import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ApiService {
+
+    @POST("api/auth/login")
+    Call<ApiResponse> login(@Body LoginRequest request);
+    @POST("api/auth/logout")
+    Call<ApiResponse> logout(@Query("userId") Long userId);
+
     @POST("api/auth/register")
     Call<ApiResponse> register(@Body RegistrationRequest request);
 
