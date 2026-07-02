dab301d3c43eb36b1e247494a8d8ab527059dacf
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2026-07-02 19:44:04 +0200

    Created JwtAuthenticationFilter that extends OncePerRequestFilter

---
+++ backend/team20-Tim-Djordjina/src/main/java/rs/ac/uns/ftn/kzi_nastava/team20_Tim_Djordjina/security/JwtAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package rs.ac.uns.ftn.kzi_nastava.team20_Tim_Djordjina.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider){
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
