f53d9726737b6bda6c33758193e28dab0a60be61
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2026-07-02 19:51:27 +0200

    Added method for token extraction

--- backend/team20-Tim-Djordjina/src/main/java/rs/ac/uns/ftn/kzi_nastava/team20_Tim_Djordjina/security/JwtAuthenticationFilter.java
+++ backend/team20-Tim-Djordjina/src/main/java/rs/ac/uns/ftn/kzi_nastava/team20_Tim_Djordjina/security/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -22,6 +23,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = extractToken(request);
+        } catch (Exception e){
+            SecurityContextHolder.clearContext();
+            logger.warn("JWT authentication failed: " + e.getMessage());
+        }
+    }
 
+    private String extractToken(HttpServletRequest request){
+        String header = request.getHeader(AUTH_HEADER);
+        if (header != null && header.startsWith(BEARER_PREFIX)){
+            return header.substring(BEARER_PREFIX.length()).trim();
+        }
+        return null;
     }
 }
